a8aacdd10908fcd759fd9f22ff77cb4e31601c6f
Romain BOURDON <romain@ayeba.fr>
2011-10-16 21:41:17 +0200

    test

---
+++ conf/config.php
@@ -0,0 +1,20 @@
+<?php
+
+/** 
+ * fichier de configuration de l'application
+ * 
+ * 
+ * @package  
+ * @author 
+ */
+ 
+    
+    define('SQL_SERVER','localhost');
+    define('SQL_DB','formation2');
+    define('SQL_USER','root');
+    define('SQL_PASS','root');
+    
+
+    
+    
+   ini_set('display_errors','on');
